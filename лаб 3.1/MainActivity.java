1ff6435820254e8162a905c60f197db0ef59da44
vika-babenko <62217114+vika-babenko@users.noreply.github.com>
2021-05-24 16:52:26 +0300

    additional task

--- лаб 3.1/MainActivity.java
+++ лаб 3.1/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,16 +22,22 @@ public class MainActivity extends AppCompatActivity {
         EditText edNumber = findViewById(R.id.input);
         TextView tw = findViewById(R.id.textView);
         int number = Integer.parseInt(edNumber.getText().toString());
+        long start=System.nanoTime();
         if (number % 2 == 0) {
            tw.setText(number/2+"*2");
         }
         else {
         int x = (int) Math.ceil(Math.sqrt(number));
-        
+
         while (!(Math.pow((int) Math.sqrt(x * x - number), 2) == x * x - number)) {
             x += 1;
         }
         int y = (int) Math.sqrt(x * x - number);
+        if ((System.nanoTime()-start)>Math.pow(10,9)){
+            Snackbar.make(view,"Час виконання програми перевищує 1 секунду і становить "+
+                    (System.nanoTime()-start)+ " наносекунд.",Snackbar.LENGTH_LONG).show();
+        }
+        else
         tw.setText((x-y)+"*"+(x+y));
     }}
 
